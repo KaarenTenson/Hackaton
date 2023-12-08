5ee65f7a837c24623d10c704d49806bd0ad8fefe
kaaren123 <tensonkaaren@gmail.com>
2023-12-08 17:41:08 +0200

    uuendused

--- distsipliin/Form1.Designer.cs
+++ distsipliin/Form1.Designer.cs
@@ -30,12 +30,130 @@ namespace distsipliin
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sisestaRakendusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.halbToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.heaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sisestaRakendusToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sisestaRakendusToolStripMenuItem
+            // 
+            this.sisestaRakendusToolStripMenuItem.Name = "sisestaRakendusToolStripMenuItem";
+            this.sisestaRakendusToolStripMenuItem.Size = new System.Drawing.Size(129, 24);
+            this.sisestaRakendusToolStripMenuItem.Text = "sisesta rakendus";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(305, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DISTSIPLIIN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sisesta rakendus";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.halbToolStripMenuItem,
+            this.heaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(111, 52);
+            // 
+            // halbToolStripMenuItem
+            // 
+            this.halbToolStripMenuItem.Name = "halbToolStripMenuItem";
+            this.halbToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
+            this.halbToolStripMenuItem.Text = "Halb";
+            // 
+            // heaToolStripMenuItem
+            // 
+            this.heaToolStripMenuItem.Name = "heaToolStripMenuItem";
+            this.heaToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
+            this.heaToolStripMenuItem.Text = "Hea";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "timer:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sisestaRakendusToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem halbToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem heaToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- distsipliin/Form1.cs
+++ distsipliin/Form1.cs
@@ -12,9 +12,35 @@ namespace distsipliin
 {
     public partial class Form1 : Form
     {
+        public int timer = 0;
         public Form1()
         {
+            
             InitializeComponent();
+            label3.Text = timer.ToString();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="t"){
+                Console.WriteLine("Hello World"); }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        public void label3_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+        
+
     }
 }
