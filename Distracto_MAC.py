10b7aa2fd12cf8038415279db36558cf796b4872
andersaa1 <ander.saarniit@gmail.com>
2023-12-09 00:07:41 +0200

    kivy test fail ja pythoni script

---
+++ Distracto_MAC.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+# READING APPS FROM '/applications' DIRECTORY
+def get_installed_applications():
+    try:
+        applications_directory = "/Applications"
+        files = os.listdir(applications_directory)
+        executable_files = [file for file in files if os.access(os.path.join(applications_directory, file), os.X_OK)]
+        return executable_files
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+app_count = 0
+installed_apps_array = list()
+installed_apps = get_installed_applications()
+if installed_apps:
+    for app in installed_apps:
+        installed_apps_array.append(app)
+        app_count += 1
+
+print("There are "+str(app_count)+" apps installed on your computer.\nYour installed apps are:")
+print(installed_apps_array)
+
+# FUNCTION FOR OPENING APPS
+def open_application(application_name):
+    try:
+        subprocess.run(['open', '-a', application_name])
+        print(f"{application_name} opened successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+open_application("")
+
--- script_MAC.py
+++ script_MAC.py
@@ -1,20 +1,13 @@
 import os
 def get_installed_applications():
     try:
-        # Specify the directory to search for applications
         applications_directory = "/Applications"
-
-        # Get a list of files in the specified directory
         files = os.listdir(applications_directory)
-
-        # Filter files to include only those with executable permissions
         executable_files = [file for file in files if os.access(os.path.join(applications_directory, file), os.X_OK)]
-
         return executable_files
     except Exception as e:
         print(f"Error: {e}")
         return None
-
 
 installed_apps = get_installed_applications()
 if installed_apps:
